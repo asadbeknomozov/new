67fac08c0de1df6233337d8a868eb7cc86903b4a
Asadbek <nomozovasadbek149@gmail.com>
2026-07-24 08:51:49 +0500

    22.07.2026

--- uyishi_mock.py
+++ uyishi_mock.py
@@ -67,6 +67,23 @@
 # print(ends_with_gram(words))
 
 
+#               5-masala.
+
+# def get_phone_number(contacts: dict[str, str], search_name: str) -> str:
+#     for i in contacts:
+#         if i == search_name:
+#             return contacts[i]
+
+# contacts = {
+#  "Ali": "+998901112233",
+#  "Vali": "+998909998877",
+#  "Hasan": "+998938889900"
+# }
+
+# search_name = "Vali"
+# print(get_phone_number(contacts, search_name))
+
+
 
 
 
